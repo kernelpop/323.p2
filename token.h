bf48fd51d924f8894c4c625be1f673a653e85d50
David Feinzimer <dfeinzimer@csu.fullerton.edu>
2017-04-19 14:30:53 -0700

    Add token.h

---
+++ token.h
@@ -0,0 +1,10 @@
+#include <string>
+
+using namespace std;
+
+struct Token {
+	int ln;			// line number
+	string id;		// id based on grammar
+	int ix;			// optional; index number
+	string str;		// optional; actual string
+};
