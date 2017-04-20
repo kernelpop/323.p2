6f283d7b14ef62c41804b3de3235a99914347161
Daniel Bravo <bravoappdev@gmail.com>
2017-04-19 21:52:19 -0700

    Added SymTable class.

---
+++ SymTable.cpp
@@ -0,0 +1,26 @@
+struct occurence {
+    int lineNum;
+    int tokenNum;
+    bool isDef;
+}
+
+class Sym {
+    int ix;
+    string printName;
+    bool isString;
+    double valueDouble;
+    string valueString;
+    list<occurence> occurences;
+public:
+    Sym();
+    ~Sym();
+    
+};
+
+class SymTable {
+    list<Sym> table;
+public:
+    SymTable();
+    ~SymTable();
+    
+};
