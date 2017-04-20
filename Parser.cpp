1a3ea99d9560bd784a02114edec0d73f8d702e6c
Daniel Bravo <bravoappdev@gmail.com>
2017-04-19 21:35:07 -0700

    Changed structure to use Node's insteads separate PST and AST classes.

--- Parser.cpp
+++ Parser.cpp
@@ -3,58 +3,114 @@
 #include <map>
 #include "Token.cpp"
 #include "Grammar.cpp"
-#include "PST.cpp"
-#include "AST.cpp"
+#include "SymTable.cpp"
 
 using namespace std;
 
-
 class Parser {
 	Grammar gmr;
-	PST pst;
-	AST ast;
+	Node* pst;
+	Node* ast;
+	SymTable symTable;
+	vector<Token> tokenList;
 
-	Parser(PST rPst, AST rAst, Grammar rGmr) {
-		pst = rPst;
-		ast = rAst;
+	Parser(Grammar rGmr) {
 		gmr = rGmr;
+		symTable = new SymTable();
 	}
 
 	Parser() {
-		pst = new PST();
-		ast = new AST();
 		gmr = new Grammer();
 	}
 
 	/**
-	    Parse a token list into an Abstract Syntax Tree
+	    Add a list of Token to be parsed
 
 	    @param tokenList a vector list of the tokens to parse
 	*/
-	void parse(vector<Token> tokenList) {
-
-		// Lexar input to PST
-		pstTree pTreeObj = pst.toAST(tokenList);
-		pTreeObj.tree.serialize(); // save and print out
-		map<Symbol, String> symTable = pTreeObj.symTable; // save and print out
-
-		// PST to AST
-		astTree aTree = ast.parse(ptree);
-		aTree.serialize(); // save and print out
+	void addTokensToParse(vector<Token> rTokenList) {
+		tokenList = rTokenList;
 	}
 
 	/**
-	    Overload: Parse a token list from a file into an Abstract Syntax Tree
-
-	    @param the path to the file to parse
+	    Make a Parse Tree from the current token list.
 	*/
-	void parse(String file) {
-		vector<Token> tknList = toTokenList(file);
-		parse(tknList);
+	void makePST() {
+		// Setup:
+		stack<Symbol> workingStack;
+		list<Token> inputStream(tokenList);
+
+		workingStack.push(Symbol("$"));		// Add the eof symbol
+		inputStream.push_back(Token("$"));	// Add the eof token
+
+		workingStack.push(Symbol("Pgm"));	// Add the start symbol
+		// pst = new Node(workingStack.top());
+
+		while(!workingStack.empty()) {
+			// int i = 0;
+			Symbol top = workingStack.top();
+			Token front = inputStream.front();
+
+			if(top.isTerminal) {
+				if(top.name != front.id) {
+					// TODO: throw an error
+					// Error: epected x, found y, line front.line
+				}
+			}
+
+			// M1:
+			list<Symbol> rule = gmr.getRule(top.name, front.id);
+			if(!rule.empty()) {
+				// Remove the top symbol from stack
+				workingStack.pop();
+				
+				// Add the rule backwards
+				for (int i = rule.size() - 1; i >= 0; --i) {
+					workingStack.push(rule[i]);
+				}
+
+				// Reassign the top variable
+				top = workingStack.top();
+			} else {
+				// The rule is empty => there is no prediction for this
+
+				// TODO: throw an error
+				// Error: unexpected token found, line front.line
+			}
+
+			// M2
+			// else if(top.isTerminal {
+			// 	Error();
+			// }
+
+			// // M3
+			// else if(gmr.getRule(top, front).isEmpty()){
+			// 	Error();
+			// }
+
+			// // M4
+			// else if(!gmr.getRule(top, front).isEmpty()) {
+			// 	workingStack.pop();
+			// 	workingStack.push(gmr.getRule(top, front).reverse());
+
+			// }
+			// else {
+			// 	printStatus();
+			// }
+		}
 	}
 
-	vector<Token> toTokenList(String file) {
-		// TODO: turn file into Token list
+	void makeAST() {
+
 	}
 
- }
+	/**
+	    Take the tree and create a printable represenation of it
+
+	    @param tree a node pointer to the tree to serialize
+	*/
+	string serializeTree(Node* tree) {
+		// TODO: Serialize the tree
+	}
+
+}
