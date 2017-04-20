96fe8b3adeb02119623ba2ff68a42ab460f1b59a
Daniel Bravo <bravoappdev@gmail.com>
2017-04-19 21:41:41 -0700

    Added basic Node functions. Insert, find, remove.

--- Node.cpp
+++ Node.cpp
@@ -1,10 +1,39 @@
 class Node {
-    Symbol value;
+    Symbol* content;
     list<Node*> children;
 public:
-    Node(Symbol rValue) {
-        value = rValue;
+    Node(Symbol rContent) {
+        content = rContent;
     }
     ~Node();
+
+    void insert(Node* newChild) {
+        children.insert(newChild);
+    }
+
+    Node find(string node) {
+        if (children.empty()) {
+            if (content.name == node) {
+                return content
+            } else {
+                return NULL;
+            }
+        }
+
+        for (int i = 0; i < children.size(); ++i){
+            children[i].find(node);
+        }
+
+
+
+    }
+
+    void remove(Node* child) {
+        children.remove(child);
+    }
+
+    string toString() {
+
+    }
     
 };
