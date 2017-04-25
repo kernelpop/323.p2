de63657900d1d1bf292c7a342a34828de57f5979
Daniel Bravo <bravoappdev@gmail.com>
2017-04-24 19:12:20 -0700

    Added functions to tokenize and parse.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "tokenize.cpp"
 #include "token.h"
 #include "txt_to_strings.cpp"
+#include "Parser.cpp"
 
 using namespace std;
 
 int main() {
+    vector<Token> tokens = runTokenizer();
+    runParser(tokens);
 	return 0;
 }
+
