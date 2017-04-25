a3a253a1ed8aea5c0744bfbec7cf4239833a1592
Daniel Bravo <bravoappdev@gmail.com>
2017-04-24 20:04:24 -0700

    Added function prototypes.

--- token.h
+++ token.h
@@ -8,3 +8,14 @@ struct Token {
 	int ix;			// optional; index number
 	string str;		// optional; actual string
 };
+
+//  ========  Function Prototypes  ========
+vector<Token> tokenize(vector<string> lines);
+bool isLetter(char c, bool firstChar);
+bool isNumber(char c, bool firstChar);
+string whichKwd(string s);
+string whichNumType(string s);
+Token tokenate(int ln, string id, int ix, string str);
+Token tokenate(int ln, string id, string str);
+Token tokenate(int ln, string id);
+static const string NOTKWD("NOTKWD");
