84cace9eb5386f53f34b90360d2ce8a3e9a811ca
David Feinzimer <dfeinzimer@csu.fullerton.edu>
2017-04-27 00:43:05 -0700

    Refined main.cpp Relocated most content from resources.h into their own files Added token_printer

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "token.h"
 #include "Parser.cpp"
 #include "resources.h"
+#include "txt_to_strings.cpp"
+#include "token_printer.cpp"
+
 
 using namespace std;
+
 
 //	========  Global Variables  ========
 string _Language = "A3";
@@ -13,58 +17,70 @@ vector<string> _Strings;
 
 
 //	========  Function Prototypes  ========
-vector<Token> runTokenizer();
 void runParser(vector<Token>);
+
 
 int main() {
 	
-	_Tokens = runTokenizer();
-	//runParser(_Tokens);
-
-	return 0;
-}
-
-vector<Token> runTokenizer() {
 	int x = 0;
 
-	cout << "Please input a test number or a negative number to exit: ";
-	cin >> x;
+	while (true) {
+		
+		cout << "Please input a test number (1-6) or any negative number to exit: ";
+		
+		cin >> x;
+		
+		if (x < 0) {	
+			break;
+		}
 
-	if (x < 0) {
-		exit(0);
+		Test_File = "test_" + to_string(x) + ".txt";
+		
+
+		if (!cin.fail()) {	
+
+			ifstream f(Test_File.c_str());                       
+			
+			if (f.good()) {
+				
+				cout << "========  Start Test - " << Test_File << " ========" << endl;
+
+				_Strings = txt_to_strings(Test_File);
+
+				_Tokens = tokenize(_Strings);
+
+				token_printer(_Language, _Tokens);
+
+				runParser(_Tokens);
+
+				_Strings.clear();
+
+				_Tokens.clear();
+
+				cout << "========  End Test  ========" << endl;
+				
+			}                                                    
+			
+			else {
+				
+				cout << "No such file found in current directory!" << endl;
+			
+			}
+		
+		}
+		
+		else {
+			
+			cout << "Bad input detected." << endl;
+			
+			cin.clear();
+			
+			cin.ignore(INT_MAX, '\n');
+		
+		}
+	
 	}
+	
+	return 0;
 
-	Test_File = "test_" + to_string(x) + ".txt";
-
-	//  Checks for correct input
-	if (!cin.fail())
-	{
-		//  Checks if a file is available
-		ifstream f(Test_File.c_str());                       //If true run lexer
-		if (f.good())
-		{
-			// ========  Start Test  ========
-			cout << "========  Start Test - " << Test_File << " ========" << endl;
-
-
-			_Strings = txt_to_strings(Test_File);
-
-			_Tokens = tokenize(_Strings);
-
-			printer(_Language, _Tokens);
-
-			_Strings.clear();
-
-
-			cout << "========  End Test  ========" << endl;
-			// ========  End Test  ========
-		}                                                    //  Else output error
-		else cout << "No such file found in current directory!" << endl;
-	}
-	else //  If output is not correct, display message and clear input stream
-	{
-		cout << "Wrong input detected." << endl;
-		cin.clear();
-		cin.ignore(INT_MAX, '\n');
-	}
 }
